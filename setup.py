5b2063d618a795ab3f522949c5f1e626eaae23bf
kubrasadeb <kubrasade.b@gmail.com>
2024-12-13 11:36:18 +0300

    Add: Zip download feature

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-report-builder",
-    version="6.5.7",
+    version="6.6.0",
     author="David Burke",
     author_email="david@burkesoftware.com",
     maintainer="Mustafa Ülker",
@@ -15,18 +15,18 @@ setup(
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires='>=3.4',
+    python_requires=">=3.4",
     install_requires=[
-        'openpyxl>=3.1.0',
-        'python-dateutil',
-        'djangorestframework>=3.8.0',
-    ]
+        "openpyxl>=3.1.0",
+        "python-dateutil",
+        "djangorestframework>=3.8.0",
+    ],
 )
